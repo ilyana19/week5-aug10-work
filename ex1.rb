292faf97e7aa7bb0de9a7a895bbf59427119dbf0
ilyana19 <aries_2152@hotmail.com>
2018-08-10 12:41:29 -0400

    finished part 2

--- ex1.rb
+++ ex1.rb
@@ -8,7 +8,18 @@ seats = [
 seats.each_with_index {|row_val, row_index|
   seats[row_index].each_with_index {|col_val, col_index|
     if col_val == nil
-      puts "Row #{row_index+1} seat #{col_index+1} is free"
+      print "Row #{row_index+1} seat #{col_index+1} is free. "
+      print "Do you want to sit there? (y/n) "
+      user_input = gets.chomp.downcase
+
+      if user_input == 'y'
+        print "What is your name? "
+        user_name = gets.chomp.capitalize
+        seats[row_index][col_index] = user_name
+      end
     end
   }
 }
+
+puts ""
+p seats
